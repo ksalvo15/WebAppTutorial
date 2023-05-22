29dbd371524b82ae9c50055cf3e2cc1a65d65dad
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-05-22 13:34:25 -0700

    added some JS

--- WebAppTutorial/wwwroot/js/site.js
+++ WebAppTutorial/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
